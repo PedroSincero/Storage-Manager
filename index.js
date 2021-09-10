bf40d845eb9ae7a5a1c0920ee549f780d3a77db0
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-09-10 15:01:31 -0300

    START - Adicionando conecçoes e index - [DONE]

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
 app.get('/', (_request, response) => {
-  response.send();
+  response.send('ola, estou funcionando');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+ console.log(`Ouvindo a porta ${PORT}`);
 });
